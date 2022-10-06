8e6d6b1f5ccd979dbafaabaf5be2cf61a6930c23
Lyndsay Futrell <lyndsayfutrell@gmail.com>
2022-10-06 15:51:57 -0400

    finished tshirt step

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const nameField = document.getElementById("name");
 const otherField = document.getElementById("other-job-role");
 const jobField = document.getElementById("title");
 const shirtColor = document.getElementById("color");
+const shirtColorOption = document.querySelectorAll("#color option");
 const shirtDesign = document.getElementById("design");
 
 //Focus name field on page load
@@ -26,8 +27,23 @@ shirtDesign.addEventListener("change", e=> {
     const value = e.target.value;
     if(value === "js puns") {
         shirtColor.removeAttribute('disabled', '');
+        shirtColorOption[0].setAttribute('hidden', '');
+        shirtColorOption[1].removeAttribute('hidden', '');
+        shirtColorOption[2].removeAttribute('hidden', '');
+        shirtColorOption[3].removeAttribute('hidden', '');
+        shirtColorOption[4].setAttribute('hidden', '');
+        shirtColorOption[5].setAttribute('hidden', '');
+        shirtColorOption[6].setAttribute('hidden', '');
+
     } else if (value === "heart js") {
         shirtColor.removeAttribute('disabled', ''); 
+        shirtColorOption[0].setAttribute('hidden', '');
+        shirtColorOption[1].setAttribute('hidden', '');
+        shirtColorOption[2].setAttribute('hidden', '');
+        shirtColorOption[3].setAttribute('hidden', '');
+        shirtColorOption[4].removeAttribute('hidden', '');
+        shirtColorOption[5].removeAttribute('hidden', '');
+        shirtColorOption[6].removeAttribute('hidden', '');
     } else {
         shirtColor.setAttribute('disabled', '');
     }
